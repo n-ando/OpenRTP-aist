39a845399c56aab18fecd2948b76d7146d6007b1
Sakamoto Takeshi <sakamoto@globalassist.co.jp>
2020-01-23 13:37:16 +0900

    Modified LogView.#266

--- jp.go.aist.rtm.systemeditor/src/jp/go/aist/rtm/systemeditor/ui/util/LoggerHandler.java
+++ jp.go.aist.rtm.systemeditor/src/jp/go/aist/rtm/systemeditor/ui/util/LoggerHandler.java
@@ -1,39 +1,43 @@
 package jp.go.aist.rtm.systemeditor.ui.util;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.ui.PlatformUI;
 import org.msgpack.value.Value;
 
 import influent.EventEntry;
 import influent.forward.ForwardCallback;
 import influent.forward.ForwardServer;
+import jp.go.aist.rtm.systemeditor.ui.views.logview.LogParam;
 
 public class LoggerHandler {
 	private ForwardServer logServer;
-	private Text logOut;
+	private List<LogParam> logList;
+	private TableViewer logTable;
 	
 	ForwardCallback callback = ForwardCallback.ofSyncConsumer(
 	  stream -> {
-		  String tag = stream.getTag().getName();
+//		  String tag = stream.getTag().getName();
 		  for (EventEntry entry : stream.getEntries()) {
-		      long time = entry.getTime().toEpochMilli();
-		      String json = entry.getRecord().toJson();
-			  System.out.println();
-			  System.out.println("time:" + time);
-			  System.out.println("json:" + json);
+			  String strRaw = entry.getTime().toString() + " " + entry.getRecord().toJson();
+//		      long time = entry.getTime().toEpochMilli();
+//		      String json = entry.getRecord().toJson();
+//			  System.out.println();
+//			  System.out.println("time:" + time);
+//			  System.out.println("json:" + json);
 			  
 		      Value[] val = entry.getRecord().getKeyValueArray();
 		      for(Value each : val) {
 		    	  if(each.toString().equals("log")) continue;
-				  System.out.println("logContents:" + each.toString());
+//				  System.out.println("logContents:" + each.toString());
 			      PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
 						public void run() {
-						  String exist = logOut.getText();
-						  logOut.setText(exist + System.lineSeparator() + each.toString());
+							logList.add(new LogParam(each.toString(), strRaw));
+							logTable.refresh();
 						}
 					});
 		      }
@@ -42,8 +46,9 @@ public class LoggerHandler {
 	  Executors.newFixedThreadPool(1)
 	);
 	
-	public void startServer(int portNo, final Text target) {
-		this.logOut = target;
+	public void startServer(int portNo, List<LogParam> logList, TableViewer logTable) {
+		this.logTable = logTable;
+		this.logList = logList;
 		logServer = new ForwardServer
 				  .Builder(callback)
 				  .localAddress(portNo)
