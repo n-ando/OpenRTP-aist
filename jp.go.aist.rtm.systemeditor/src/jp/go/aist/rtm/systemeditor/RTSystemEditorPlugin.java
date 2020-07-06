957299f7045aefd025d5b8a49825259a4065360f
Sakamoto Takeshi <sakamoto@globalassist.co.jp>
2020-07-07 00:02:21 +0900

    Modified to display a warning message when toolbar setting failed at startup.#88

--- jp.go.aist.rtm.systemeditor/src/jp/go/aist/rtm/systemeditor/RTSystemEditorPlugin.java
+++ jp.go.aist.rtm.systemeditor/src/jp/go/aist/rtm/systemeditor/RTSystemEditorPlugin.java
@@ -1,9 +1,17 @@
 package jp.go.aist.rtm.systemeditor;
 
+import jp.go.aist.rtm.systemeditor.nl.Messages;
 import jp.go.aist.rtm.toolscommon.profiles.util.LoggerUtil;
 
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
@@ -39,6 +47,15 @@ public class RTSystemEditorPlugin extends AbstractUIPlugin {
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		
+		IWorkspaceRoot workspaceHandle = ResourcesPlugin.getWorkspace().getRoot();
+		IPath targetFile = workspaceHandle.getRawLocation().append(".metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi");
+		if( targetFile.toFile().exists() == false ) {
+			IWorkbench workbench = PlatformUI.getWorkbench();
+			IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+			MessageDialog.openError(window.getShell(), "RTSystemEditor", Messages.getString("SystemEditor.1"));
+			return;
+		}
 	}
 
 	/*
